f4b0d1746d167a1f6617de2f4adbe0dad349c1b7
beliayam <beliayamcom@gmail.com>
2021-08-18 13:02:30 +0700

    production pos back-end v1.0.3 beta

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return "Halo Hackers... :)";
 });
 
 //$router->group(['middleware' => 'cors'], function () use ($router){
